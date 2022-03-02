9d35cec32a98b2cbcb7633120e0a3f849cb5654c
Humberto Petita <humbertopetita@gmail.com>
2022-03-02 20:44:58 -0300

    olá querido

--- exericio - um/Program.cs
+++ exericio - um/Program.cs
@@ -6,7 +6,18 @@ namespace exericio___um
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe o valor em R$: ");
+            string valorDigitado = Console.ReadLine();
+            Decimal valorConvertido = 0.0m;
+            decimal valorCambio = 0.1932m;
+
+            decimal.TryParse(valorDigitado, out valorConvertido);
+
+            decimal valorCambioConvertido = valorConvertido / valorCambio;
+
+            Console.WriteLine($"Valor em $: (valorCambioConvertido)");
+            Console.ReadLine();
+
         }
     }
 }
