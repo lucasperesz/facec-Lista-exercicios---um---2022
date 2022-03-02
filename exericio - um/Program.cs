442d7241d9a72dd8a2ea8b715d832739327ac5da
Humberto Petita <humbertopetita@gmail.com>
2022-03-02 20:25:35 -0300

    bem vindo

---
+++ exericio - um/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exericio___um
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
