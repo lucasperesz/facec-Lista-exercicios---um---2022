b9bc7ddc23e06220cce3248ede74a0381353b1bd
Humberto Petita <humbertopetita@gmail.com>
2022-03-09 20:53:39 -0300

    Att switch Calculadora

--- exercicio - cinco/Program.cs
+++ exercicio - cinco/Program.cs
@@ -20,35 +20,60 @@ namespace exercicio___cinco
                 Console.WriteLine("4 => divisão");
                 Console.WriteLine("5 => sobra da divisão");
                 string operacao = Console.ReadLine();
+                int operacaoInt = Convert.ToInt32(operacao);
 
                 decimal valorUm = 0.0m;
                 decimal valorDois = 0.0m;
                 decimal.TryParse(valorUmStr, out valorUm);
                 decimal.TryParse(valorDoisStr, out valorDois);
 
-                if (operacao.Equals("1"))
+                #region IF
+                //if (operacao.Equals("1"))
+                //{
+                //    Console.WriteLine($"Resultado: {valorUm}+{valorDois}={(valorUm + valorDois)}");
+                //}
+                //else if (operacao.Equals("2"))
+                //{
+                //    Console.WriteLine($"Resultado: {valorUm}-{valorDois}={(valorUm - valorDois)}");
+                //}
+                //else if (operacao.Equals("3"))
+                //{
+                //    Console.WriteLine($"Resultado: {valorUm}x{valorDois}={(valorUm * valorDois)}");
+                //}
+                //else if (operacao.Equals("4"))
+                //{
+                //    Console.WriteLine($"Resultado: {valorUm}/{valorDois}={(valorUm / valorDois)}");
+                //}
+                //else if (operacao.Equals("5"))
+                //{
+                //    Console.WriteLine($"Resultado: {valorUm}/{valorDois}=> Resto={(valorUm % valorDois)}");
+                //}
+                #endregion IF
+
+                #region SWITCH
+                switch (operacaoInt)
                 {
-                    Console.WriteLine($"Resultado: {valorUm}+{valorDois}={(valorUm + valorDois)}");
+                    case 1:
+                        Console.WriteLine($"Resultado: {valorUm}+{valorDois}={(valorUm + valorDois)}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Resultado: {valorUm}-{valorDois}={(valorUm - valorDois)}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Resultado: {valorUm}x{valorDois}={(valorUm * valorDois)}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Resultado: {valorUm}/{valorDois}={(valorUm / valorDois)}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"Resultado: {valorUm}/{valorDois}=> Resto={(valorUm % valorDois)}");
+                        break;
                 }
-                else if (operacao.Equals("2"))
-                {
-                    Console.WriteLine($"Resultado: {valorUm}-{valorDois}={(valorUm - valorDois)}");
-                }
-                else if (operacao.Equals("3"))
-                {
-                    Console.WriteLine($"Resultado: {valorUm}x{valorDois}={(valorUm * valorDois)}");
-                }
-                else if (operacao.Equals("4"))
-                {
-                    Console.WriteLine($"Resultado: {valorUm}/{valorDois}={(valorUm / valorDois)}");
-                }
-                else if (operacao.Equals("5"))
-                {
-                    Console.WriteLine($"Resultado: {valorUm}/{valorDois}=> Resto={(valorUm % valorDois)}");
-                }
+                #endregion SWITCH
                 Console.WriteLine("Deseja Continuar? s/n");
                 repetir = Console.ReadLine();
                 Console.WriteLine("");
+                
             }
 
 
