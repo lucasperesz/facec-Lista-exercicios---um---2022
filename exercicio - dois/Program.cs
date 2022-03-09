cd00ec36a8d84777f4efb8d35f00415867ac1530
Humberto Petita <humbertopetita@gmail.com>
2022-03-09 19:59:43 -0300

    exercicio 4

---
+++ exercicio - dois/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio___dois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem Vindo!");
+            static void Main()
+            {
+                Console.Write("Valor em Reais? ");
+                float reais = float.Parse(Console.ReadLine());
+
+                double usd = reais / 5.22;
+
+                Console.WriteLine("$ = " + usd);
+            }
+        }
+    }
+}
---
+++ exercicio - quatro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio___quatro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o ano de nascimento!");
+            string nascimentoStr = Console.ReadLine();
+            int idade;
+            int anoNascimento;
+            int anoAtual = DateTime.Today.Year;
+
+            int.TryParse(nascimentoStr, out anoNascimento);
+            idade = (anoAtual - anoNascimento);
+
+            Console.WriteLine($"A idade é {idade}");
+        }
+    }
+}
