9bb8b9883106066735487415da577a72339becf8
Humberto Petita <humbertopetita@gmail.com>
2022-03-25 18:57:47 -0300

    aulass 2022 --

---
+++ Exercicio 09 - Nove/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercicio_09___Nove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Livro("Dois Irmãos", "Milton Hatoum", 99.90m).ToString());
+            Console.ReadLine();
+        }
+    }
+}
